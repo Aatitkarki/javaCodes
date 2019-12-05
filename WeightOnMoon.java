1c82f5a720038275aa554a5fa26f45c706fe7e9b
Aatitkarki <52074509+Aatitkarki@users.noreply.github.com>
2019-12-05 19:24:06 +0545

    Create WeightOnMoon.java

---
+++ WeightOnMoon.java
@@ -0,0 +1,24 @@
+//WAP to calculate the effective weight on moon.
+import java.util.Scanner;
+public class WeightOnMoon{
+	public static int weightIn(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your weight on moon");
+		int a=sc.nextInt();
+		weight(a);
+	}
+
+	public static float weight(int a){
+		float moonWeight = a * (17/100);
+		return moonWeight;
+	}
+
+	public static void main(Strings args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your weight on moon");
+
+
+		System.out.println("Your weight on moon is "+)
+
+	}
+}
