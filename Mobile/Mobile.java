a2da1a1e96adec8e80a14f6f77284b8a0d591677
Aatitkarki <52074509+Aatitkarki@users.noreply.github.com>
2019-12-05 19:27:17 +0545

    new files

--- Mobile/Mobile.java
+++ Mobile/Mobile.java
@@ -1,78 +1,87 @@
 public class Mobile{
-	private String companyName;
-	private String mobileName;
-	private int modelNumber;
-	private byte ram;
-	private int internalStorage;
-	private double screenSize;
-	static int count;
-	private boolean expandableStorage;
+    private String companyName;
+    private String mobileName;
+    private String modelNumber;
+    private byte ram;
+    private int internalStorage;
+    private double screenSize;
+    static int count;
+    private boolean expandableStorage;
 
-	public String getCompanyName(){
-		return companyName;
-	}
-	public String setCompanyName(String cn){
-		companyName=n;
-	}
-	public String getMobileName(){
-		return mobileName;
-	}
-	public String setMobileName(String mn){
-		mobileName=mn;
-	}
-	public int getModelNumber(){
-		return modelNumber;
-	}
-	public int setModelNumber(int mn){
-		modelNumber=mn;
-	}
-	public byte getRam(){
-		return ram;
-	}
-	public byte setRam(byte r){
-		ram=r;	
-	}
-	public int getInternalStorage(){
-		return internalStorage;
-	}
-	public int setInternalStorage(int is){
-		internalStorage=is;
-	}
-	public double getScreenSize(){
-		return screenSize;
-	}
-	public double setScreenSize(double ss){
-		screenSize=ss;
-	}
-	public boolean getExpandableStorage(){
-		return expandableStorage;
-	}
-	public boolean setExpandableStorage(boolean es){
-		expandableStorage=es;
-	}
-	public Mobile(String companyName,String mobileName,int modelNumber,byte ram,int internalStorage,double screenSize,boolean expandableStorage){
-		count+=1;
-		this.companyName=companyName;
-		this.mobileName=mobileName;
-		this.modelNumber=modelNumber;
-		this.ram=ram;
-		this.internalStorage=internalStorage;
-		this.screenSize=screenSize;
-		this.expandableStorage=expandableStorage;
+    public String getCompanyName(){
+        return companyName;
+    }
+    public void setCompanyName(String cn){
+        companyName=cn;
+    }
+    public String getMobileName(){
+        return mobileName;
+    }
+    public void setMobileName(String mn){
+        mobileName=mn;
+    }
+    public String getModelNumber(){
+        return modelNumber;
+    }
+    public void setModelNumber(String mn){
+        modelNumber=mn;
+    }
+    public byte getRam(){
+        return ram;
+    }
+    public void setRam(byte r){
+        ram=r;  
+    }
+    public int getInternalStorage(){
+        return internalStorage;
+    }
+    public void setInternalStorage(int is){
+        internalStorage=is;
+    }
+    public double getScreenSize(){
+        return screenSize;
+    }
+    public void setScreenSize(double ss){
+        screenSize=ss;
+    }
+    public boolean getExpandableStorage(){
+        return expandableStorage;
+    }
+    public void setExpandableStorage(boolean es){
+        expandableStorage=es;
+    }
+    public Mobile(String companyName,String mobileName,String modelNumber,byte ram,int internalStorage,double screenSize,boolean expandableStorage){
+        count+=1;
+        this.companyName=companyName;
+        this.mobileName=mobileName;
+        this.modelNumber=modelNumber;
+        this.ram=ram;
+        this.internalStorage=internalStorage;
+        this.screenSize=screenSize;
+        this.expandableStorage=expandableStorage;
 
-	}
-	public void displayDetails(){
-		System.out.println("Company Name: "+companyName);
-		System.out.println("Mobile Name: "+mobileName);
-		System.out.println("Model Number: "+modelNumber);
-		System.out.println("Ram size: "+ram);
-		System.out.println("Internal Storage: "+internalStorage);
-		System.out.println("Screen Size: "+screenSize);
-		if (expandableStorage==true){
-			System.out.println("Expandable storage is avilable.");
-		}else{
-			System.out.println("Expandable storage is not avilable.")
-		}
-		
-	}
+    }
+    public void displayDetails(){
+        System.out.println("Company Name: "+companyName);
+        System.out.println("Mobile Name: "+mobileName);
+        System.out.println("Model Number: "+modelNumber);
+        System.out.println("Ram size: "+ram+"GB");
+        System.out.println("Internal Storage: "+internalStorage+"GB");
+        System.out.println("Screen Size: "+screenSize+" inch.");
+        if (expandableStorage==true){
+            System.out.println("Expandable storage is avilable.");
+        }else{
+            System.out.println("Expandable storage is not avilable.");
+        }
+        System.out.println();
+    }
+    
+    public boolean isSame(Mobile mobile){
+        boolean isSame=false;
+                if(getModelNumber() == mobile.getModelNumber()){
+        isSame=true;
+        }
+        return isSame;
+    }
+
 }
--- Mobile/MobileDemo.java
+++ Mobile/MobileDemo.java
@@ -1,5 +1,50 @@
 public class MobileDemo{
-	public static void main(){
-		String mobile[]=String [30];
-	}
+    public static void main(String[] args){
+        Mobile mobiles[]=new Mobile [30];
+        Mobile s8 = new Mobile("Samsung","s8","SMG950",(byte)4,64,5.8,true);
+        
+        Mobile s9 = new Mobile("Samsung","S9+","G960F",(byte)6,64,6.2,true);
+        
+        Mobile s10 =new Mobile("Samsung","S10","SMG97",(byte)8,128,6.1,true);
+        
+        Mobile iPhone6s =new Mobile("Apple","iPhone 6s","A1633",(byte)2,16,5.5,false);
+        
+        Mobile iPhone8 =new Mobile("Apple","iPhone 8+","A1633",(byte)3,64,5.5,false);
+        
+        Mobile iPhoneX =new Mobile("Apple","iPhone X","A1865",(byte)3,64,5.8,false);
+        
+        Mobile iPhone11=new Mobile("Apple","iPhone 11","A2221",(byte)4,64,6.1,false);
+        
+        Mobile k20Pro = new Mobile("Xiaomi","Redmi k20 Pro","M1903F11",(byte)6,64,6.39,false);
+        
+        Mobile nova5T= new Mobile("Huawei","Nova 5T","YAL-L21",(byte)6,128,6.26,false);
+        
+        Mobile p30 =new Mobile("Huawei","P30","ELE-TL00",(byte)8,64,6.1,true);
+        
+        mobiles[0]=s8;
+        mobiles[1]=s9;
+        mobiles[2]=s10;
+        mobiles[3]=iPhone6s;
+        mobiles[4]=iPhone8;
+        mobiles[5]=iPhoneX;
+        mobiles[6]=iPhone11;
+        mobiles[7]=k20Pro;
+        mobiles[8]=nova5T;
+        mobiles[9]=p30;
+    
+        System.out.println("The total smartphone is "+Mobile.count);
+        System.out.println();
+        
+        for(int i=0; i<10;i++){
+        if(mobiles[i].getCompanyName()=="Apple"){
+            mobiles[i].displayDetails();
+            }
+        }
+        System.out.println("The details of Mobiles having 4GB ram and 64GB internal storage are");
+        for(int i=0;i<10;i++){
+        if(mobiles[i].getRam()==4 && mobiles[i].getInternalStorage()==64){
+        mobiles[i].displayDetails();
+        }
+    }
+    }
 }
